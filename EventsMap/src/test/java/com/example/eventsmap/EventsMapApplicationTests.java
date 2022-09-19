ce33869ddd4130cc7470dfaac16b14e50937dc6c
kushevsky01 <78921029+kushevsky01@users.noreply.github.com>
2022-09-19 17:51:46 +0300

    Add files via upload

---
+++ EventsMap/src/test/java/com/example/eventsmap/EventsMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventsmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
